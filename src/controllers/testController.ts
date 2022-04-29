502dafa982dba2fb243bc0127b960ccd00098aaa
ramon oliveira <ramon30012000@gmail.com>
2022-04-29 14:32:43 -0300

    feat: add search by discipline and teacher

--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -2,13 +2,17 @@ import { Request, Response } from "express";
 import testService from "../services/testService.js";
 
 async function find(req: Request, res: Response) {
-  const { groupBy } = req.query as { groupBy: string };
+  const { groupBy, disciplineName, teacherName } = req.query as { 
+    groupBy: string;
+    disciplineName?: string; 
+    teacherName?: string; 
+  };
 
   if (groupBy !== "disciplines" && groupBy !== "teachers") {
     return res.sendStatus(400);
   }
 
-  const tests = await testService.find({ groupBy });
+  const tests = await testService.find({ groupBy, disciplineName, teacherName });
   res.send({ tests });
 }
 
